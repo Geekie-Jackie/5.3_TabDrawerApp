b9e3190f0e50aaca35ac871fe065b1872c90f2be
Jackie <lcd.jackson@gmail.com>
2023-11-11 08:32:19 +0800

    initial commit - upload 5.3 assignment

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-export default function App() {
+// Details Screen
+function DetailsScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Details!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Home Screen
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+// Settings Screen
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+// Home Stack Navigator
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+// Settings Stack Navigator
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+// Bottom Tab Navigator
+const Tab = createBottomTabNavigator();
+
+// Drawer Navigator
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        {/* Drawer Screen for Home */}
+        <Drawer.Screen name="Home">
+          {() => (
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+              <Tab.Screen name="Home" component={HomeStackScreen} />
+              <Tab.Screen name="Settings" component={SettingsStackScreen} />
+            </Tab.Navigator>
+          )}
+        </Drawer.Screen>
+
+        {/* Drawer Screen for Settings */}
+        <Drawer.Screen name="Settings">
+          {() => (
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+              <Tab.Screen name="Home" component={HomeStackScreen} />
+              <Tab.Screen name="Settings" component={SettingsStackScreen} />
+            </Tab.Navigator>
+          )}
+        </Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// Tab.Navigator is now wrapped inside the Drawer.Screen. This ensures that there's only one navigator directly under the NavigationContainer.
